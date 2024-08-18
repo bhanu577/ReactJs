7703d07201c246604c7eafe02e5f50cd63dd3711
bhanu577 <dammalapatibhanu@gmail.com>
2024-08-18 22:36:19 +0530

    add ep2ReactJs

--- App.js
+++ App.js
@@ -1,10 +1,5 @@
-// <div id="parent">
-//      <div id="child>"
-//          <h1> iam inside h1 >/h1>  
-//      </div>
-//</div
-//
-//
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement("div", { id: "parent" },
     [React.createElement("div", { id: "child" },
         [React.createElement("h1", {}, "I am inside h1"), React.createElement("h2", {}, "I am inside h2")]

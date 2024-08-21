c40c4554b90155542d0163a1e45df9ae8253be85
bhanu577 <dammalapatibhanu@gmail.com>
2024-08-21 17:34:19 +0530

    Add functional component

--- App.js
+++ App.js
@@ -1,8 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxheading = (<h1 id="heading">Namaste React using JSX
-    Namaste NodeJS
-    Namaste JavaScript</h1>);
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      <h1 className="heading">Namaste React using Functionl Component</h1>
+    </div>
+  );
+};
+const Heading2Component = () => (
+    <div>
+    <h2>Hi Iam From Heading2Component</h2>
+    </div>
+);
+const JsxHeading = () => (
+  <div>
+    <h1 id="heading">
+      Namaste React using JSX Namaste NodeJS Namaste JavaScript
+    </h1>
+    <HeadingComponent />
+    <Heading2Component />
+  </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxheading);
+root.render(<JsxHeading />);
